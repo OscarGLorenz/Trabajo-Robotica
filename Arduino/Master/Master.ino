d0e51f6ebdb6187d4a28a28a1921203718994f56
Unknown <miferco97@gmail.com>
2018-10-09 12:28:37 +0200

    home y fin de carrera

--- Arduino/Master/Master.ino
+++ Arduino/Master/Master.ino
@@ -1,31 +1,32 @@
 #include <Wire.h>
 
-void setup() {
+#define ENDSTOP_PIN A0
+#define M1_adress 0x11
+#define M2_adress 0x12
+#define M3_adress 0x13
+#define STOP  "J30"
+
+void initialize() {
   Wire.begin();         // Unirse al bus 12C
   Serial.begin(115200); // Iniciar la comunicación Serial
+  pinMode(ENDSTOP_PIN, INPUT);
+
 }
 
-/*
-  void serialSend (String code, int device) {
+void setup() {
+  initialize();
+}
 
+void SerialSend (String code, int device) {
   Wire.beginTransmission(device);
-  for (i = 0; i < code.length(); i++) {
-    Wire.write((char) code[i]);
-  }
+  for(int i=0;i<code.length();i++)Wire.write(code[i]);
+  delayMicroseconds(10);
   Wire.endTransmission();
-
-  }*/
+}
 
 void loop() {
-  // Si llega algo por serial, mandar el comando 0x01 y el ángulo
-  Wire.beginTransmission(0x10); // transmit to device #8
-  Wire.write("S3 2.65 ");             // sends one byte
-  Wire.endTransmission();    // stop transmitting
-
-  delay(1000);
-  /*Wire.beginTransmission(0x10); // transmit to device #8
-  Wire.write('D');             // sends one byte
-  Wire.endTransmission();    // stop transmitting
-  delay(1000);
-*/
+  if (analogRead(ENDSTOP_PIN) < 150)SerialSend(STOP, M1_adress);
+  else {
+    SerialSend("J3 100", M1_adress);
+  }
 }
