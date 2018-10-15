70567ca354d0f07f3d932beb656148c0d89149db
Hector <libaobao897@gmail.com>
2018-10-15 13:24:21 +0200

    Master actualizado

--- Arduino/prueba comunicacion I2C completa/MasterI2C/MasterI2C.ino
+++ Arduino/prueba comunicacion I2C completa/MasterI2C/MasterI2C.ino
@@ -1,18 +1,72 @@
 #include <Wire.h>
 
 #define ENDSTOP_PIN A0
-#define M1_adress 0x11
-#define M2_adress 0x12
-#define M3_adress 0x13
+#define M1_address 0x11
+#define M2_address 0x12
+#define M3_address 0x13
 #define STOP  "J30"
 
 void initialize() {
   Wire.begin();         // Unirse al bus 12C
   Serial.begin(115200); // Iniciar la comunicación Serial
   pinMode(ENDSTOP_PIN, INPUT);
-
 }
+
 int sig = 1;
+char Nmotor;
+char Type;
+int ID_action;
+int address;
+String aux_MSG;
+float Dato[3];
+
+void resetParam() {
+  Type = '0';
+  ID_action = 0;
+  Nmotor = 0;
+  ID_action = 0;
+}
+
+void m_address() {
+  if (Nmotor == '1') {
+    address = M1_address;
+  }
+  else if (Nmotor == '2') {
+    address = M2_address;
+  }
+  else if (Nmotor == '3') {
+    address = M3_address;
+  }
+}
+
+void process_MSG(String mensaje) {
+
+  int i = 0;
+  String ID;
+  ID = "";
+  resetParam();
+
+  //Captura el motor
+  mensaje.remove(0, 1);
+  Nmotor = mensaje[0];
+  mensaje.remove(0, 2);
+
+  //Mensaje para mandar al esclavo
+  aux_MSG = mensaje;
+
+  //captura el modo
+  Type = mensaje[0];
+  mensaje.remove(0, 1);
+  i = 0;
+
+  //captura el ID
+  while (mensaje[i] != ' ' && mensaje.length() >= i) {
+    ID += mensaje[i];
+    i++;
+  }
+
+  ID_action = ID.toInt();
+}
 
 void setup() {
   initialize();
@@ -27,33 +81,35 @@ void I2C_Send (String code, int device) {
 
 //Realizar la request
 
-void makeRequest() {
-  Wire.requestFrom(M1_adress, 1);
-  while (Wire.available()) {
-    sig =(int) Wire.read();
+void makeRequest(int device) {
+  if (Type == 'S') {
+    Wire.requestFrom(device, 1);
+    while (Wire.available()) {
+      sig = (int) Wire.read();
+    }
+  }
+  else if (Type == 'R') {
+    Wire.requestFrom(device, sizeof(float));
+    while (Wire.available()) {
+      Dato[nmotor] = Wire.parseFloat();
+    }
+    sig = 1;
   }
 }
-
 void loop() {
   if (analogRead(ENDSTOP_PIN) < 150) {
-    I2C_Send(STOP, M1_adress);
+    I2C_Send(STOP, M1_address);
     delay(10);
-    makeRequest();
+    makeRequest(M1_address);
   }
   else {
-    if (sig == 1) {
-      I2C_Send("J3 100", M1_adress);
-      sig = 0;
-      
+    if (Type == 'S') {
+      I2C_Send(aux_MSG, address);
     }
-
     else {
-      makeRequest();
+      makeRequest(address);
       delay(10);
     }
-
   }
-
-
   Serial.println(sig);
 }
