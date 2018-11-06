9b67a61e6178cbfd13daf8cf98b7ee5879ff7f2c
Hector <libaobao897@gmail.com>
2018-11-06 11:14:56 +0100

    Comentado

--- Arduino/Interrupciones/Interrupciones.ino
+++ Arduino/Interrupciones/Interrupciones.ino
@@ -2,12 +2,11 @@ boolean toggle2 = 0;
 void setup() {
   pinMode(9, OUTPUT);
   noInterrupts();//stop interrupts
-  //set timer2 interrupt at 8kHz
   TCCR2A = 0;// set entire TCCR2A register to 0
   TCCR2B = 0;// same for TCCR2B
   TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+  //Cambiar OCR2A para cambiar la frecuencia
+  OCR2A = 249;// = (16*10^6) / (frequency*8) - 1 (must be <256)
   // turn on CTC mode
   TCCR2A |= (1 << WGM21);
   // Set CS21 bit for 8 prescaler
@@ -19,8 +18,8 @@ void setup() {
 
 }//end setup
 
-ISR(TIMER2_COMPA_vect) { //timer1 interrupt 8kHz toggles pin 9
-  //generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
+ISR(TIMER2_COMPA_vect) { //timer1 interrupt toggles pin 9
+  //generates pulse wave of frequency/2 (takes two cycles for full wave- toggle high then toggle low)
   if (toggle2) {
     digitalWrite(9, HIGH);
     toggle2 = 0;
