447c0222a9bd0358b2b4c5ca58a328bd42f79b1f
Óscar García Lorenz <ogl@gomezdemercado.es>
2018-12-10 09:02:01 +0100

    Paletizar

---
+++ Matlab/Paletizado.m
@@ -0,0 +1,98 @@
+global ardu;
+global x;
+global y;
+global z;
+    
+
+ardu = serial('/dev/ttyACM1','BaudRate',115200);
+ardu.Terminator = 'LF';
+ardu.OutputBufferSize = 1024;
+ardu.BytesAvailableFcnMode =  'terminator';
+ardu.BytesAvailableFcn = @(~,~) serialEvent;
+        
+
+
+
+fopen(ardu);
+
+
+disp("PULSA HOME");
+pause;
+fprintf(ardu,"J0");
+
+sss = 6;
+
+pos = zeros(sss,sss,3);
+
+disp("VAMOS A 30");
+pause;
+fprintf(ardu,"J1 M1 30");
+
+disp("PULSA MODO GÜIDADO");
+pause;
+fprintf(ardu,"J6");
+
+disp("GUARDA PUNTO 1");
+pos(1,1,:) = [x y z];
+pause;
+
+
+disp("GUARDA PUNTO 2");
+pos(1,sss,:) = [x y z];
+pause;
+
+
+disp("VAMOS A 220");
+pause;
+fprintf(ardu,"J6");
+pause(1);
+fprintf(ardu,"J1 M1 220");
+
+disp("PULSA MODO GÜIDADO");
+pause;
+fprintf(ardu,"J6");
+
+disp("GUARDA PUNTO 3");
+pos(sss,1,:) = [x y z];
+pause;
+
+fprintf(ardu,"J6");
+
+xx = linspace(pos(1,1,1),pos(sss,1,1),sss);
+yy = linspace(pos(1,1,2),pos(1,sss,2),sss);
+
+[X, Y] = meshgrid(xx,yy);
+pos(:,:,1)=X;
+pos(:,:,2)=flipud(Y);
+pos(:,:,3)=-75;
+
+disp("PULSA PA PALETISAH");
+
+pause;
+w1 = [pos(2,2,1) pos(2,2,2) pos(2,2,3)]
+w2 = [pos(4,4,1) pos(4,4,2) pos(4,4,3)]
+
+paletizar([x y z],w1,w2);
+
+
+disp(" SE ACABO");
+pause;
+fclose(ardu);
+
+
+function serialEvent()
+global ardu;
+global x;
+
+global y;
+global z;
+r = fscanf(ardu,"%f %f %f");
+[x,y,z]=directa(r(1),deg2rad(r(2)),deg2rad(r(3)));
+
+end
+
+
+
+
+
+
---
+++ Matlab/paletizar.m
@@ -0,0 +1,61 @@
+function paletizar(ahora,desde,hasta)
+global ardu;
+
+offset = 30;
+p = 3;
+T = 10;
+
+wp1 = ahora;
+wp2 = desde; wp2(3) = wp2(3) + offset;
+wp3 = desde;
+wp4 = hasta; wp4(3) = wp4(3) + offset;
+wp5 = hasta; 
+
+move(wp1,wp2,T,p);      pause(T);
+fprintf(ardu,"J4 M2 255");
+move(wp2,wp3,T/4,p);      pause(T/4);
+move(wp3,wp2,T/4,p);      pause(T/4);
+
+move(wp2,wp4,T,p);      pause(T);
+
+move(wp4,wp5,T/4,p);      pause(T/4);
+fprintf(ardu,"J4 M2 0");
+move(wp5,wp4,T/4,p);      pause(T/4);
+
+move(wp4,wp1,T,p);      pause(T);
+
+end
+
+
+function move(x0,xf,T,p)
+global ardu;
+
+% Muestrear en el espacio de la tarea
+x1 = linspace(x0(1),xf(1),p);
+x2 = linspace(x0(2),xf(2),p);
+x3 = linspace(x0(3),xf(3),p);
+
+% Muestrar el tiempo
+t = linspace(0,T,p);
+
+% Cinemática inversa de los puntos
+[q1,q2,q3] = inversa(x1,x2,x3);
+
+q2 = rad2deg(q2);
+q3 = rad2deg(q3);
+
+% Obtener los polinomios para coordenada articular con pendiente nula al
+% principio y al final
+pp1 = spline(t,[0 q1 0]);
+pp2 = spline(t,[0 q2 0]);
+pp3 = spline(t,[0 q3 0]);
+
+% Generar mensaje en formato  d1 d2 d3 c1 c2 c3 b1 b2 b3 a1 a2 a3 t1 t2 t3
+str1 = sprintf(' %3.3f',[pp1.coefs(:);t(2:end)']);
+str2 = sprintf(' %3.3f',[pp2.coefs(:);t(2:end)']);
+str3 = sprintf(' %3.3f',[pp3.coefs(:);t(2:end)']);
+
+command = sprintf('J5 %d%s_%d%s_%d%s_\n', p-1, str1, p-1, str2, p-1, str3);
+
+fprintf(ardu,command);
+end
